044dfc53937df8f5d3c89e0e13e2b6cbdc4480c0
Everton Cabral <evertoncab@gmail.com>
2021-02-19 23:44:07 -0300

    configurando o setup

---
+++ pokedex-vue/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	lintOnSave: true,
+	css: {
+		loaderOptions: {
+			sass: {
+				additionalData: `
+                @import "@/styles/functions";
+                @import "@/styles/colors";
+                `,
+			},
+		},
+	},
+};
