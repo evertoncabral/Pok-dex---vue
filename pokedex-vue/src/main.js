882caf5a68ebd1fff67795f04d55b46e496617c3
Everton Cabral <evertoncab@gmail.com>
2021-02-25 22:21:53 -0300

    desenvolvendo list parte 6

--- pokedex-vue/src/main.js
+++ pokedex-vue/src/main.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
-import "./styles/main.scss";
+import InfiniteLoading from "vue-infinite-loading";
 import App from "./App.vue";
-import InfiniteLoading from 'vue-infinite-loading';
+
+import "./styles/main.scss";
 
 Vue.config.productionTip = false;
 
 Vue.use(InfiniteLoading, {});
-
 
 new Vue({
 	render: h => h(App)
