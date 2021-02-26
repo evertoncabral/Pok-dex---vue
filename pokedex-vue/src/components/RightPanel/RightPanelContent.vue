0d9ba6405317a3567748e82c1de9f6081d4773b1
Everton Cabral <evertoncab@gmail.com>
2021-02-25 22:36:34 -0300

    desenvolvendo list parte 7

--- pokedex-vue/src/components/RightPanel/RightPanelContent.vue
+++ pokedex-vue/src/components/RightPanel/RightPanelContent.vue
@@ -1,39 +1,65 @@
 <template>
-  <form action="" class="search">
-    <label for="search" class="search_label text--white bg-black">
-      <h3 class="search_label--text">E.g. : Charizard</h3>
-      <input id="search" type="text" class="search_input" />
+  <div class="search">
+    <label for="search" class="search__label text--white bg--black">
+      <h3 class="search__label--text">E.g.: Charizard | 6</h3>
+      <input
+        id="search"
+        v-model.trim="name"
+        type="text"
+        class="search__input"
+        @keyup.enter="searchPokemon"
+      />
     </label>
 
-    <button class="search_button bg-gray text--yellow" @click.prevent>
-      Search Fake
-    </button>
-  </form>
+    <div class="buttons">
+      <button class="btn btn--clear" @click.prevent="clear">Clear</button>
+
+      <button
+        class="btn btn--search bg--gray text--yellow"
+        @click.prevent="searchPokemon"
+      >
+        Search
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { actions, mutations } from "@/store";
+
 export default {
   name: "RightPanelContent",
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    clear() {
+      this.name = "";
+      mutations.resetList();
+    },
+    async searchPokemon() {
+      await actions.searchPokemon(this.name);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .search {
   display: flex;
-  flex-direction: colum;
+  flex-direction: column;
   justify-content: space-between;
   flex: 1;
   transform: rotateX(-180deg);
-
   @media (min-width: $viewport-medium) {
     transform: rotateY(-180deg);
-
     padding-top: 50px;
   }
-
-  &_label {
+  &__label {
     display: flex;
-    flex-direction: colum;
+    flex-direction: column;
     justify-content: space-between;
     width: 100%;
     height: 50%;
@@ -44,31 +70,39 @@ export default {
       padding: 12px 24px 24px;
     }
   }
-
-  &_input {
+  &__input {
     color: inherit;
     background: transparent;
     border: none;
     border-bottom: 2px solid color(white);
     padding: 4px 8px;
-
     @media (min-width: $viewport-medium) {
       padding: 8px;
     }
   }
-  &_button {
-    align-self: flex-end;
-    width: 120px;
-    height: 50px;
-    border: 4px solid color(black);
-    border-radius: 8px;
-    font-size: 18px;
-    font-weight: bold;
-    cursor: pointer;
-
-    @media (min-width: $viewport-medium) {
-      width: 160px;
-      height: 60px;
+  .buttons {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    .btn {
+      border-radius: 8px;
+      font-size: 18px;
+      font-weight: bold;
+      cursor: pointer;
+      &--clear {
+        width: 80px;
+        padding: 8px;
+        margin-right: 16px;
+        border: none;
+      }
+      &--search {
+        width: 120px;
+        padding: 15px;
+        border: 4px solid color(black);
+        @media (min-width: $viewport-medium) {
+          width: 160px;
+        }
+      }
     }
   }
 }
